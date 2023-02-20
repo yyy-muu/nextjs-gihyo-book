a3bbfb418385b5dfdef32a3f2839d1db5de6b85b
yyy-muu <rirakkuma.0918.y@gmail.com>
2023-02-20 14:27:32 +0900

    Moleculesコンポーネントを追加

--- src/components/layout/Box/index.tsx
+++ src/components/layout/Box/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import styled from 'styled-components'
 import type { Responsive } from 'types/styles'
-import { toPropValue, Color, Space } from 'utils/styles'
+import { toPropValue, Color, Space } from '/src/utils/styles'
 
 export type BoxProps = {
   color?: Responsive<Color>
